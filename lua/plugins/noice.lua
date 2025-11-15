b9498cc543bb2c5326817f38da396aa864ed9156
kwmiebach <kwmiebach@users.noreply.github.com>
2025-11-15 10:51:18 +0100

    Move confirmation dialog to the bottom

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,14 +1,23 @@
--- Override noice.nvim command line popup position to bottom
+-- Override noice.nvim popup positions to bottom area
 return {
   "folke/noice.nvim",
   opts = {
     views = {
+      -- Confirmation dialogs (y/n/cancel)
+      confirm = {
+        position = {
+          row = "85%", -- Position near bottom of screen
+          col = "50%", -- Keep centered horizontally
+        },
+      },
+      -- Command line popup
       cmdline_popup = {
         position = {
           row = "90%", -- Position near bottom of screen
           col = "50%", -- Keep centered horizontally
         },
       },
+      -- Command line popup menu
       cmdline_popupmenu = {
         position = {
           row = "80%", -- Position the popup menu above the cmdline
